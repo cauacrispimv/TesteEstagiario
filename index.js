0a9c506458bca19a70d7495aa0203ce83d5e8a33
Cauã Crispim <cauacrispimvieira@gmail.com>
2024-12-08 00:27:45 -0300

    Commit 2.0

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-let btnSubmit = document.getElementById('btnSubmit');
+function verificaCampos(event) {
+  let nome = document.getElementById("nome").value;
+  let email = document.getElementById("email").value;
+  let telefone = document.getElementById("telefone").value;
+
+  if (nome === "" || email === "" || telefone === "") {
+    alert("Todos os campos são obrigatórios.");
+    event.preventDefault();
+  } else {
+    console.log(nome, email, telefone);
+  }
+}
+
+let form = document.getElementById("formJB");
+form.addEventListener("submit", verificaCampos);
